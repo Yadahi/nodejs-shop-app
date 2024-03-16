d50215665a3214b1451d5e0629acea1f8d27cf3d
Viliam <vatvil@gmail.com>
2024-03-16 16:52:45 +0100

    creating session

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,6 @@ const authController = require("../controllers/authController");
 
 router.get("/login", authController.getLogin);
 
+router.post("/login", authController.postLogin);
+
 module.exports = router;
