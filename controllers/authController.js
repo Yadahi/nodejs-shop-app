d7c320e6f82fa86fd674b6aa29465bcadaea5f01
Viliam <vatvil@gmail.com>
2024-03-17 21:36:05 +0100

    add signup view

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,18 @@
 const User = require("../models/user");
 
 const getLogin = (req, res, next) => {
-  console.log("isLoggedIn", req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
     isAuthenticated: req.session.isLoggedIn,
+  });
+};
+
+const getSignup = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    isAuthenticated: false,
   });
 };
 
@@ -27,13 +34,19 @@ const postLogin = (req, res, next) => {
     });
 };
 
-const getLogout = (req, res, next) => {
-  req.session.destroy();
-  res.redirect("/");
+const postSignup = (req, res, next) => {};
+
+const postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
 };
 
 module.exports = {
   getLogin,
+  getSignup,
   postLogin,
-  getLogout,
+  postSignup,
+  postLogout,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const router = express.Router();
+
 const authController = require("../controllers/authController");
+
+const router = express.Router();
 
 router.get("/login", authController.getLogin);
 
+router.get("/signup", authController.getSignup);
+
 router.post("/login", authController.postLogin);
 
-router.post("/logout", authController.getLogout);
+router.post("/signup", authController.postSignup);
+
+router.post("/logout", authController.postLogout);
 
 module.exports = router;
