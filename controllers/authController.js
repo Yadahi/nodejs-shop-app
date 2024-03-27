7553dd8141e18b464a276bd8adf5138df99cc563
Viliam <vatvil@gmail.com>
2024-03-27 21:42:33 +0100

    add reset passowrd controller and view

--- controllers/authController.js
+++ controllers/authController.js
@@ -169,6 +169,37 @@ const postReset = (req, res, next) => {
   });
 };
 
+const getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+
+  User.findOne({
+    resetToken: token,
+    resetTokenExpiration: { $gt: Date.now() },
+  })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      // TODO check if token is not expired
+      if (!user) {
+        req.flash("error", "No account with that email found.");
+        return res.redirect("/reset");
+      }
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        errorMessage: message,
+        userId: user._id.toString(),
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 module.exports = {
   getLogin,
   getSignup,
@@ -177,4 +208,5 @@ module.exports = {
   postLogout,
   getReset,
   postReset,
+  getNewPassword,
 };
