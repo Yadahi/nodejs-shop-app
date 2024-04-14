d623ecc1121ff56c8b4b3bc59290c2ef2ddca279
Viliam <vatvil@gmail.com>
2024-04-14 21:28:27 +0200

    download file

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
@@ -148,6 +150,20 @@ const getCheckout = (req, res, next) => {
   });
 };
 
+const getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+  const invoiceName = "invoice-" + orderId + ".pdf";
+  const invoicePath = path.join("data", "invoices", invoiceName);
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", "inline; filename=" + invoiceName);
+    res.send(data);
+  });
+};
+
 module.exports = {
   getProducts,
   getProduct,
@@ -158,4 +174,5 @@ module.exports = {
   postOrder,
   getOrders,
   postCardDeleteProduct,
+  getInvoice,
 };
